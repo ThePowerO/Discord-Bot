d1bdcaf5e2ee6007d9df23841e385e192af351e8
ThePowerO <136516815+ThePowerO@users.noreply.github.com>
2023-07-22 22:33:40 -0300

    Update DiscordBot.py

--- DiscordBot.py
+++ DiscordBot.py
@@ -97,3 +97,4 @@ async def playersAF2(ctx):
                 players_element = soup.find('p', {'class': 'text-lead font-caption-body wait-for-i18n-format-render'})
                 players_count = players_element.text.strip()
                 await ctx.reply(f"The current number of players playing Anime Fighters 2 is {players_count}")
+            else:
