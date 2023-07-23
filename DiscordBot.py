49abfed24441a3756ed169b0dffa79c2baee19b9
ThePowerO <136516815+ThePowerO@users.noreply.github.com>
2023-07-22 22:36:56 -0300

    Update DiscordBot.py

--- DiscordBot.py
+++ DiscordBot.py
@@ -110,5 +110,6 @@ async def playersArsenal(ctx):
                 soup = BeautifulSoup(html, 'html.parser')
                 players_element = soup.find('p', {'class': 'text-lead font-caption-body wait-for-i18n-format-render'})
                 players_count = players_element.text.strip()
-                await ctx.reply(f"The current number of players playing The Strongest Battle Ground is {players_count}")
+                await ctx.reply(f"The current number of players playing Arsenal is {players_count}")
             else:
+                await ctx.reply("Unable to get the number of players at the moment.")
