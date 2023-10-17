cf210d5c4fc0bf5af3e35eef3d76a72a3b1ccbbb
ThePowerO <136516815+ThePowerO@users.noreply.github.com>
2023-10-16 23:05:34 -0300

    Update joinppl.py

--- joinppl.py
+++ joinppl.py
@@ -10,12 +10,30 @@ layout = [
     [sg.Button("Find"), sg.Button("Exit")]
 ]
 
-while True:
-    username = input("Insert the roblox player username: ")
-    open_roblox_profile(username)
+window = sg.Window("Roblox Profile Finder", layout)
 
-    repeat = input("Want to find someone else? (y/n): ")
-    if repeat.lower() in ['n', 'no']:
+while True:
+    event, values = window.read()
+
+    if event in (sg.WIN_CLOSED, 'Exit'): #or if event == 'Exit: break
         break
+    if event == 'Find':
+        username = values['username']
+        open_roblox_profile(username)
 
 window.close()
+
+# Old code
+#import webbrowser
+
+#def open_roblox_profile(username):
+#    profile_url = f"https://www.roblox.com/users/profile?username={username}"
+#    webbrowser.open(profile_url)
+
+#while True:
+#    username = input("Insert the roblox player username: ")
+#    open_roblox_profile(username)
+
+#    repeat = input("Want to find someone else? (y/n): ")
+#    if repeat.lower() in ['n', 'no']:
+#        break
