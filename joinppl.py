711b9c89701b768036f796ca2fdac7cdde412751
ThePowerO <136516815+ThePowerO@users.noreply.github.com>
2023-10-16 23:05:15 -0300

    Update joinppl.py

--- joinppl.py
+++ joinppl.py
@@ -5,6 +5,11 @@ def open_roblox_profile(username):
     profile_url = f"https://www.roblox.com/users/profile?username={username}"
     webbrowser.open(profile_url)
 
+layout = [
+    [sg.Text("Roblox username: "), sg.InputText(key='username')],
+    [sg.Button("Find"), sg.Button("Exit")]
+]
+
 while True:
     username = input("Insert the roblox player username: ")
     open_roblox_profile(username)
